f53baecf9ee9f7632cc0c82afb3015e1fd9e1cb0
hoya <hybriddevslim@gmail.com>
2022-02-26 15:52:22 +0900

    - 음료수 구매, 음료수 등록 command parameter 추가

--- src/main/kotlin/vendor1/command/BuyDrink.kt
+++ src/main/kotlin/vendor1/command/BuyDrink.kt
@@ -1,7 +1,9 @@
 package vendor1.command
 
+import vendor1.drink.DrinkName
+
 /**
  * 음료 구매 커맨드
  */
-class BuyDrink {
+class BuyDrink(val name: DrinkName, val amount: Int) {
 }
--- src/main/kotlin/vendor1/command/RegisterDrink.kt
+++ src/main/kotlin/vendor1/command/RegisterDrink.kt
@@ -1,7 +1,12 @@
 package vendor1.command
 
+import vendor1.drink.DrinkName
+
 /**
  * 음료수 등록
  */
-class RegisterDrink {
-}
+class RegisterDrink(
+    val name: DrinkName,
+    val price: Int,
+    val count: Int
+)
