2b07faaad66efa832489c534d3c1103a72d77b64
hoya <hybriddevslim@gmail.com>
2022-02-27 03:14:53 +0900

    - 소켓 서버 구성 - 커맨드 입력 받으면 이벤트 실행

--- src/main/kotlin/vendor1/VendorApplication.kt
+++ src/main/kotlin/vendor1/VendorApplication.kt
@@ -1,11 +1,13 @@
 package vendor1
 
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
-import kotlinx.coroutines.*
-import vendor1.command.VendorOperation
 import vendor1.vendor.Vendor
 import vendor1.vendor.VendorOperationService
+import java.io.*
+import java.net.ServerSocket
+import java.net.Socket
 import java.util.*
+import kotlin.concurrent.thread
 
 
 object SingletonClass {
@@ -14,23 +16,62 @@ object SingletonClass {
 }
 
 val mapper = jacksonObjectMapper()
+fun main() {
+    val server = ServerSocket(9999)
+    println("Open Server - ${server.localPort} -")
+    while (true) {
+        val client = server.accept()
+        println("Client connected: ${client.inetAddress.hostAddress}")
+        thread { ClientListener(client).connect() }
+    }
+}
 
-fun main() = runBlocking {
-    val vendorOperationService = SingletonClass.vendorOperationService
-    val job = GlobalScope.launch {
-        while (true) {
-            val base64Command = readLine() ?: ""
-            val command = String(Base64.getDecoder().decode(base64Command))
-            vendorOperationService.setVendorStatus(command)
-            vendorOperationService.registerDrink(command)
-            vendorOperationService.printSpecification(command)
-            vendorOperationService.buyDrink(command)
+class ClientListener(
+    private val client: Socket
+) {
+    private val reader: InputStream = client.getInputStream()
+    private val writer: OutputStream = client.getOutputStream()
+    private val vendorOperationService = SingletonClass.vendorOperationService
+    private var running = false
+
+    fun connect() {
+        running = true
+        println("Vendor Drink Machine")
+        while (running) {
+            val command = read(reader)
+            if (command.isNullOrEmpty()) {
+                continue
+            }
+            val decodeCommand = String(Base64.getDecoder().decode(command))
+            vendorOperationService.setVendorStatus(decodeCommand)?.let {
+                writer.write((it + System.lineSeparator()).toByteArray(Charsets.UTF_8))
+                writer.flush()
+            }
+            vendorOperationService.registerDrink(decodeCommand)?.let {
+                writer.write((it + System.lineSeparator()).toByteArray(Charsets.UTF_8))
+                writer.flush()
+            }
+            vendorOperationService.printSpecification(decodeCommand)?.let {
+                writer.write((it + System.lineSeparator()).toByteArray(Charsets.UTF_8))
+                writer.flush()
+            }
+            vendorOperationService.buyDrink(decodeCommand)?.let {
+                writer.write((it + System.lineSeparator()).toByteArray(Charsets.UTF_8))
+                writer.flush()
+            }
+            writer.write("test${System.lineSeparator()}".toByteArray(Charsets.UTF_8))
+            writer.flush()
             if (vendorOperationService.shutdown()) {
+                running = false
+                client.close()
+                println("${client.inetAddress.hostAddress} closed the connection")
                 break
             }
+
         }
     }
-    job.join()
+
+    private fun read(inputStream: InputStream) = inputStream.bufferedReader(Charsets.UTF_8).readLine()
 }
 
 
