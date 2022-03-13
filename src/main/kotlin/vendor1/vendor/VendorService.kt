a61e489d3dcc8814a3a278899e1f7b6d0d61ce2e
hoya <hybriddevslim@gmail.com>
2022-03-13 15:08:24 +0900

    - 자판기 서비스 로직 수정: 데이터 parse 된 객체 바탕으로 처리

--- src/main/kotlin/vendor1/vendor/VendorService.kt
+++ src/main/kotlin/vendor1/vendor/VendorService.kt
@@ -19,115 +19,62 @@ class VendorService {
      * 음료수 구매 (거스름돈 출력)
      * @param payload 구매할 음료수 커맨드
      */
-    fun buyDrink(payload: String): String? {
-        try {
-            if (vendorStatus != VendorStatus.RUNNING) {
-                throw RuntimeException("invalid status")
-            }
-            val buyDrink = entityBinder<BuyDrink>(payload)
-            val balance = vendor.buyDrink(buyDrink.name, buyDrink.amount)
-            return "[구입한 음료수: ${buyDrink.name}], [잔여 금액: $balance]"
-        } catch (e: VendorException) {
-            return e.message
-        } catch (e: Exception) {
-            e.printStackTrace()
-            throw RuntimeException(e.cause)
+    fun buyDrink(payload: BuyDrink): String {
+        if (vendorStatus != VendorStatus.RUNNING) {
+            throw RuntimeException("invalid status")
         }
+
+        return try {
+            val balance = vendor.buyDrink(payload.name, payload.amount)
+            "[구입한 음료수: ${payload.name}], [잔여 금액: $balance]"
+        } catch (e: VendorException) {
+            "${e.message}"
+        }
+
     }
 
     /**
      * 음료수 등록
      * @param payload 등록할 음료수 커맨드
      */
-    fun registerDrink(payload: String): String? {
-        try {
-            if (vendorStatus != VendorStatus.MANAGEMENT) {
-                throw RuntimeException("invalid status")
-            }
-            val registerDrink = entityBinder<RegisterDrink>(payload)
-            if (registerDrink.type != ManagementType.REGISTER) {
-                return null
-            }
-            return vendor.createDrink(registerDrink)
-
-        } catch (e: Exception) {
-            return null
-            //throw RuntimeException("등록시 문제 발생")
+    fun registerDrink(payload: RegisterDrink): String {
+        if (vendorStatus != VendorStatus.MANAGEMENT) {
+            throw RuntimeException("invalid status")
         }
+        return vendor.createDrink(payload)
     }
 
     /**
      * 음료수 변경
      * @param payload 변경할 음료수 데이터
      */
-    fun modifyDrink(payload: String): String? {
-        try {
-            if (vendorStatus != VendorStatus.MANAGEMENT) {
-                throw RuntimeException("invalid status")
-            }
-            val modifyDrink = entityBinder<ModifyDrink>(payload)
-            if (modifyDrink.type != ManagementType.MODIFY) {
-                return null
-            }
-
-            return vendor.modifyDrink(modifyDrink)
-        } catch (e: Exception) {
-            return null
+    fun modifyDrink(payload: ModifyDrink): String {
+        if (vendorStatus != VendorStatus.MANAGEMENT) {
+            throw RuntimeException("invalid status")
         }
+        return vendor.modifyDrink(payload)
     }
 
     /**
      * 거래 내역서 출력
-     * @param payload String
+     * @param payload 명세서 출력
      */
-    fun printSpecification(payload: String): String? {
-        try {
-            if (vendorStatus != VendorStatus.MANAGEMENT) {
-                throw RuntimeException("invalid status")
-            }
-            val printSpecification = entityBinder<PrintSpecification>(payload)
-            if (printSpecification.type != ManagementType.SPECIFICATION) {
-                return null
-            }
-            return vendor.printSpecification()
-        } catch (e: Exception) {
-            return null
+    fun printSpecification(): String {
+        if (vendorStatus != VendorStatus.MANAGEMENT) {
+            throw RuntimeException("invalid status")
         }
+        return vendor.printSpecification()
     }
 
     /**
      * 자판기 상태 설정
      * @param payload 자판기 상태 커맨드
      */
-    fun setVendorStatus(payload: String): String? {
-        try {
-            val vendorOperation = entityBinder<VendorOperation>(payload)
-            if (vendorOperation.status == VendorStatus.QUIT) {
-                return null
-            }
-            this.vendorStatus = vendorOperation.status
-            return "[자판기 상태: $vendorStatus]"
-        } catch (e: Exception) {
-            return null
-        }
+    fun setVendorStatus(payload: VendorOperation): String {
+        this.vendorStatus = payload.status
+        return "[자판기 상태: $vendorStatus]"
     }
 
     fun getVendorStatus() = vendorStatus
 
-    private inline fun <reified T> entityBinder(payload: String): T {
-        try {
-            return mapper.readValue(payload, T::class.java)
-        } catch (e: Exception) {
-            throw RuntimeException("parse-fail")
-        }
-    }
-
-    fun quit(payload: String): Boolean {
-        return try {
-            val vendorOperation = entityBinder<VendorOperation>(payload)
-            vendorOperation.status == VendorStatus.QUIT
-        } catch (e: Exception) {
-            false
-        }
-    }
 }
