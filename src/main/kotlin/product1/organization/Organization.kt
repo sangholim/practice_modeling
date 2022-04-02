e6fd147bf45a62383f8d385e6a5e1cb93510c72e
hoya <hybriddevslim@gmail.com>
2022-04-02 13:11:51 +0900

    - 조직 모델링 생성

---
+++ src/main/kotlin/product1/organization/Organization.kt
@@ -0,0 +1,39 @@
+package product1.organization
+
+/**
+ * B2B 조직 - (account 역할)
+ */
+data class Organization (
+
+    /**
+     * 고유 번호
+     */
+    val id:String,
+
+    /**
+     * 조직 이메일
+     */
+    val email :String,
+
+    /**
+     * 이메일 인증 여부
+     */
+    val emailVerified: Boolean,
+
+    /**
+     * 휴대폰 번호
+     */
+    val phoneNumber: String,
+
+    /**
+     * 휴대폰 번호 인증 여부
+     */
+    val phoneNumberVerified: Boolean,
+
+    /**
+     * 조직의 주소
+     */
+    val address: String
+
+    // TODO Balance 잔고
+)
