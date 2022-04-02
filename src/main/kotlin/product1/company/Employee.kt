613a3c27d6c3bace23801a64f401fe9649ec8084
hoya <hybriddevslim@gmail.com>
2022-04-02 13:22:06 +0900

    - 사원 모델링 추가

---
+++ src/main/kotlin/product1/company/Employee.kt
@@ -0,0 +1,62 @@
+package product1.company
+
+/**
+ * B2B 사원 (sub-account)
+ */
+data class Employee(
+
+    /**
+     * 고유 번호
+     */
+    val id: String,
+
+    /**
+     * 회사 번호
+     */
+    val companyId: String,
+
+    /**
+     * 사원명
+     */
+    val name: String,
+
+    /**
+     * 직책
+     */
+    val position: String,
+
+    /**
+     * 이메일
+     */
+    val email: String,
+
+    /**
+     * 이메일 인증 여부
+     */
+    val emailVerified: Boolean,
+
+    /**
+     * 휴대폰 번호
+     */
+    val phoneNumber: String,
+
+    /**
+     * 휴대폰 번호 인증 여부
+     */
+    val phoneNumberVerified: Boolean,
+
+    /**
+     * 비밀번호
+     */
+    val password: String,
+
+    /**
+     * 최대 구매량
+     */
+    val purchaseLimit: Int,
+
+    /**
+     * 시스템 접근 권한
+     */
+    val roles: List<String>
+)
