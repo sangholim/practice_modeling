f3c0fb94ddd8276274c44f7f51e436f3f0e5c8f3
hoya <hybriddevslim@gmail.com>
2022-04-02 14:59:57 +0900

    - 지역 배송 모델링 추가

---
+++ src/main/kotlin/product1/shipment/ResidentialDelivery.kt
@@ -0,0 +1,39 @@
+package product1.shipment
+
+/**
+ * 지역 배송 설정
+ * 지역 배송 기준은 우편번호 범위 기준
+ */
+data class ResidentialDelivery(
+
+    /**
+     * 고유 번호
+     */
+    val id: String,
+
+    /**
+     * 배송 업체 고유 번호
+     */
+    val shipmentProviderId: String,
+
+    /**
+     * 지역 배송 이름
+     */
+    val name: String,
+
+    /**
+     * 지역 배송 지원 우편 번호 시작
+     */
+    val zipCodeFrom: Int,
+
+    /**
+     * 지역 배송 지원 우편 번호 종료
+     */
+    val zipCodeTo: Int,
+
+    /**
+     * 지역 배송비
+     */
+    val price: Int
+
+)
