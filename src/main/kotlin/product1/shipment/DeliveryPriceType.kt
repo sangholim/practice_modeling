a35f0bbb783f429430b53808f0f64ed560e553d3
hoya <hybriddevslim@gmail.com>
2022-04-10 12:41:04 +0900

    - 배송비 타입 생성

---
+++ src/main/kotlin/product1/shipment/DeliveryPriceType.kt
@@ -0,0 +1,42 @@
+package product1.shipment
+
+/**
+ * 배송비 타입
+ */
+enum class DeliveryPriceType {
+
+    /**
+     * 무료 배송비
+     */
+    FREE,
+
+    /**
+     * 고정 배송비
+     */
+    FLAT,
+
+    /**
+     * 구매 금액에 따른 부과
+     */
+    AMOUNT,
+
+    /**
+     * 구매 금액별 차등 배송
+     */
+    DIFFERENTIAL_AMOUNT,
+
+    /**
+     * 상품 무게별 차등 배송
+     */
+    DIFFERENTIAL_WEIGHT,
+
+    /**
+     * 상품 수량별 차등 배송
+     */
+    DIFFERENTIAL_COUNT,
+
+    /**
+     * 상품 수량에 비례하여 배송비 부과
+     */
+    PROPORTIONAL_COUNT
+}
