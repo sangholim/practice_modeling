f4d4b84e91c4f4426641eddb6de80a33581ea6af
hoya <hybriddevslim@gmail.com>
2022-04-10 14:42:38 +0900

    - 상품 분류 정책 문서 추가 - 상품 분류 클래스 생성

---
+++ src/main/kotlin/product1/category/Category.kt
@@ -0,0 +1,40 @@
+package product1.category
+
+/**
+ * 상품 분류
+ * 기본 진열 등록순, 추가 고도화는 나중에
+ * 실세로 하위 분류 갯수 정책은 DTO로 관리한다.
+ */
+data class Category(
+    /**
+     * 고유 번호
+     */
+    val id: String,
+
+    /**
+     * 분류명
+     */
+    val name: String,
+
+    /**
+     * 표시 여부
+     */
+    val isDisplay: Boolean,
+
+    /**
+     * 분류 타입
+     */
+    val displayTypes: List<MainDisplayType>,
+
+    /**
+     * 분류 깊이
+     * 값이 클수록 하위 분류
+     */
+    val depth: Int,
+
+    /**
+     * 상위 분류 참조 id
+     */
+    val children: List<Category>?
+
+)
---
+++ src/main/kotlin/product1/category/MainDisplayType.kt
@@ -0,0 +1,22 @@
+package product1.category
+
+/**
+ * 메인 분류 표시
+ */
+enum class MainDisplayType {
+
+    /**
+     * 대분류
+     */
+    FIRST,
+
+    /**
+     * 하위 분류
+     */
+    CHILD,
+
+    /**
+     * 상세 분류
+     */
+    OTHER
+}
