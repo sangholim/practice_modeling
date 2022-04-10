2f6bb4101aea00dc7337ae35fae66caa1e25a584
hoya <hybriddevslim@gmail.com>
2022-04-10 12:39:57 +0900

    - 배송료 청구 타입 생성

---
+++ src/main/kotlin/product1/shipment/DeliveryBillType.kt
@@ -0,0 +1,16 @@
+package product1.shipment
+
+/**
+ * 배송료 청구 기준 (무료 배송비 제외)
+ */
+enum class DeliveryBillType {
+    /**
+     * 정상 판매 가격 기준 (할인전)
+     */
+    BASE,
+
+    /**
+     * 최종 결제 금액 기준
+     */
+    PURCHASE
+}
