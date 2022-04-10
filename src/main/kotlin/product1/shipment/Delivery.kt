8620f633ba935c2ccf5402b98d935d433d0662d3
hoya <hybriddevslim@gmail.com>
2022-04-10 12:42:25 +0900

    - 배송 클래스 수정

--- src/main/kotlin/product1/shipment/Delivery.kt
+++ src/main/kotlin/product1/shipment/Delivery.kt
@@ -21,9 +21,9 @@ data class Delivery(
     val type: String,
 
     /**
-     * 배송 설정: 국내, 해외, 국개/해외
+     * 배송 설정: 국내, TODO: 해외, 국내/해외
      */
-    val regionType: String,
+    val regionType: DeliveryRegionType,
 
     /**
      * 배송지역
@@ -41,27 +41,12 @@ data class Delivery(
     val maxDay: Int,
 
     /**
-     * 배송비 설정: `무료 배송비` `고정 배송비` `구매 금액에 따른 부과` `구매 금액별 차등 배송` `상품 무게별 차등 배송료` `상품 수량별 차등 배송` `상품 수량에 비례하여 배송비 부과`
+     * 배송비 설정 타입
      */
-    val priceType: String,
+    val priceType: DeliveryPriceType,
 
     /**
-     * 상새 배송비 (무료 배송비 제외)
+     * 배송 상세 설정
      */
-    val details: List<DeliveryDetail>?,
-
-    /**
-     * 선택 상품 분류 적용/처리 (무료 배송비 제외): `제한없음` ...
-     */
-    val selectedProductType: String?,
-
-    /**
-     * 배송료 청구 기준 (무료 배송비 제외): `할인전 정상 판매 가격 기준` `최종 주문(결제) 금액 기준`
-     */
-    val billType: String?,
-
-    /**
-     * 배송비 선결제 설정 (무료 배송비 제외): `착불` `선결제` `착불/선결제`
-     */
-    val paymentType: String?
+    val detail : DeliveryDetail?
 )
