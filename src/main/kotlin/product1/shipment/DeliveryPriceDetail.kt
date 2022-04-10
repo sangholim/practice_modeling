a5951a69f4e898a37bce6a72a72a1f1e2b1d9c30
hoya <hybriddevslim@gmail.com>
2022-04-10 12:41:38 +0900

    - 배송 상세 클래스 생성

---
+++ src/main/kotlin/product1/shipment/DeliveryPriceDetail.kt
@@ -0,0 +1,22 @@
+package product1.shipment
+
+/**
+ * 상세 배송비 설정
+ */
+data class DeliveryPriceDetail (
+
+    /**
+     * 고유 번호
+     */
+    val id :String,
+
+    /**
+     * 배송비
+     */
+    val amount: Int,
+
+    /**
+     * 배송비 상품 조건
+     */
+    val term: DeliveryProductTerm?
+)
