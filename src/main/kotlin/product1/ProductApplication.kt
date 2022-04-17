bf9bd50b5e01a92ef391faa86d8df553df38cbbf
hoya <hybriddevslim@gmail.com>
2022-04-17 12:24:19 +0900

    - Application Mongodb 활성화

--- src/main/kotlin/product1/ProductApplication.kt
+++ src/main/kotlin/product1/ProductApplication.kt
@@ -2,8 +2,10 @@ package product1
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories
 
 @SpringBootApplication
+@EnableMongoRepositories
 class ProductApplication
 
 fun main(args: Array<String>) {
