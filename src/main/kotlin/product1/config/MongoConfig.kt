f20ce22430a5b64d6f1b9bb8c4c01c1dcb72e2eb
hoya <hybriddevslim@gmail.com>
2022-05-22 12:53:44 +0900

    reactive mongo 설정, library 추가

---
+++ src/main/kotlin/product1/config/MongoConfig.kt
@@ -0,0 +1,33 @@
+package product1.config
+
+import com.mongodb.ConnectionString
+import com.mongodb.reactivestreams.client.MongoClients
+import org.springframework.boot.autoconfigure.mongo.MongoProperties
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.mongodb.ReactiveMongoDatabaseFactory
+import org.springframework.data.mongodb.config.EnableReactiveMongoAuditing
+import org.springframework.data.mongodb.core.ReactiveMongoTemplate
+import org.springframework.data.mongodb.core.SimpleReactiveMongoDatabaseFactory
+import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories
+
+@Configuration
+@EnableReactiveMongoRepositories(
+    value = [
+        "product1.company",
+        "product1.category",
+    ]
+)
+@EnableReactiveMongoAuditing
+class MongoConfig(
+    private val mongoProperties: MongoProperties
+) {
+
+    @Bean
+    fun reactiveMongoClientFactory(): ReactiveMongoDatabaseFactory =
+        SimpleReactiveMongoDatabaseFactory(MongoClients.create(ConnectionString(mongoProperties.uri)), "product")
+
+    @Bean
+    fun reactiveMongoTemplate(): ReactiveMongoTemplate =
+        ReactiveMongoTemplate(reactiveMongoClientFactory())
+}
