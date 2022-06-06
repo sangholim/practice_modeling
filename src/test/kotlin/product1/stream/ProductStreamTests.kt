365d5373f4516f69fcaac7db7355e532f847ee3e
hoya <hybriddevslim@gmail.com>
2022-06-06 12:48:18 +0900

    테스트 코드 추가

--- src/test/kotlin/product1/stream/ProductStreamTests.kt
+++ src/test/kotlin/product1/stream/ProductStreamTests.kt
@@ -1,53 +1,30 @@
 package product1.stream
 
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.runBlocking
-import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
-import org.springframework.amqp.rabbit.core.RabbitTemplate
-import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter
+import org.junit.jupiter.api.assertDoesNotThrow
 import org.springframework.beans.factory.annotation.Autowired
-import product1.base.AbstractIntegrationTests
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration
+import org.springframework.boot.autoconfigure.mongo.MongoReactiveAutoConfiguration
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.cloud.stream.binder.test.InputDestination
+import org.springframework.cloud.stream.binder.test.TestChannelBinderConfiguration
+import org.springframework.messaging.support.MessageBuilder
 
-class ProductStreamTests: AbstractIntegrationTests() {
+@EnableAutoConfiguration(exclude = [MongoReactiveAutoConfiguration::class])
+@SpringBootTest(classes = [ProductStream::class, TestChannelBinderConfiguration::class])
+class ProductStreamTests {
 
     @Autowired
-    private lateinit var productStream: ProductStream
-
-    @Autowired
-    private lateinit var rabbitTemplate: RabbitTemplate
-
-    @BeforeEach
-    fun setup() = runBlocking {
-        rabbitTemplate.messageConverter = Jackson2JsonMessageConverter()
-    }
+    private lateinit var inputDestination: InputDestination
 
     /**
-     * 메시지를 큐로 발송시
-     * Rabbit MQ는 exchage를 통해 queue에 저장되고,
-     * 저장된 queue consumer 대상에게 전달한다.
-     * consumer 대상은 메시지를 받고 해당 메시지는 db에 저장된다.
+     * destination 으로 정의된 채널이 없는 경우 예외
      */
     @Test
-    fun produce() = runBlocking {
-        for (i in 1..100) {
-            productStream.sendMessage(ProductStreamPayload("name-$i"))
-        }
-        delay(5000)
+    fun couponIn() {
+        val message = MessageBuilder
+            .withPayload(ProductStreamPayload("name"))
+            .build()
+        assertDoesNotThrow { inputDestination.send(message, "product") }
     }
-
-    /**
-     * 메시지를 큐로 발송시
-     * rabbitTemplate 로 route-destination 으로 발송
-     * route-in [수신 채널]은 user-destination 로 발송
-     * consume [수신 채널]은 데이터를 db에 저장
-     */
-    @Test
-    fun route() = runBlocking {
-        for (i in 1..100) {
-            rabbitTemplate.convertAndSend("route-destination","", ProductStreamPayload("name-$i"))
-        }
-        delay(5000)
-    }
-
 }
