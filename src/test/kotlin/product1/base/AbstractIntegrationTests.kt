fedbc03b68ec5735299ed8ae94a51bc7a31c573b
hoya <hybriddevslim@gmail.com>
2022-06-06 13:34:59 +0900

    test-container 개선 - rabbit-mq-container 구동시, 설정 파일 로딩

--- src/test/kotlin/product1/base/AbstractIntegrationTests.kt
+++ src/test/kotlin/product1/base/AbstractIntegrationTests.kt
@@ -3,19 +3,26 @@ package product1.base
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.DynamicPropertyRegistry
 import org.springframework.test.context.DynamicPropertySource
+import org.testcontainers.containers.BindMode
 import org.testcontainers.containers.MongoDBContainer
 import org.testcontainers.containers.RabbitMQContainer
+import java.nio.file.Path
+import java.nio.file.Paths
+import kotlin.io.path.absolutePathString
 
 @SpringBootTest
 abstract class AbstractIntegrationTests {
 
     companion object {
+        private val dockerRabbitMqDirectory: Path = Paths.get("docker/rabbitMQ")
         val mongoDB = MongoDBContainer("mongo:4.0").apply {
             this.start()
         }
         val rabbit = RabbitMQContainer("rabbitmq:3-management")
-            .withUser("admin","admin")
-            .withPermission("/", "admin", ".*", ".*", ".*")
+            .withFileSystemBind(dockerRabbitMqDirectory.resolve("rabbitmq.conf").absolutePathString(),
+                "/etc/rabbitmq/rabbitmq.conf", BindMode.READ_ONLY)
+            .withFileSystemBind(dockerRabbitMqDirectory.resolve("definitions.json").absolutePathString(),
+                "/etc/rabbitmq/definitions.json", BindMode.READ_ONLY)
             .withExposedPorts(5672, 15672).apply {
                 this.start()
             }
