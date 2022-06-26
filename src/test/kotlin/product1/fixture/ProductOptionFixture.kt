c522f62c4685b3eefdc1075cfc1930beb1d2750c
hoya <hybriddevslim@gmail.com>
2022-06-26 11:55:11 +0900

    상품 옵션 mock 클래스 수정

--- src/test/kotlin/product1/fixture/ProductOptionFixture.kt
+++ src/test/kotlin/product1/fixture/ProductOptionFixture.kt
@@ -1,21 +1,30 @@
 package product1.fixture
 
-import org.bson.types.ObjectId
+import org.testcontainers.shaded.org.apache.commons.lang.RandomStringUtils
+import product1.product.domain.ProductOption
 import product1.product.domain.ProductOptionType
-import product1.product.dto.ProductOptionPayload
 import kotlin.random.Random
 
 object ProductOptionFixture {
 
-    val ID = ObjectId.get()
+    private val OPTION_NAMES = listOf("size", "color")
 
-    fun createPayloadByType(type: ProductOptionType): ProductOptionPayload = when (type) {
-        ProductOptionType.EXTRA -> ProductOptionPayload(type = type, price = Random.nextInt(1, 10) * 100)
-        ProductOptionType.COMBINATION -> ProductOptionPayload(
-            type = type,
-            variants = listOf(VariantFixture.createPayload(), VariantFixture.createPayload()),
-            price = Random.nextInt(1, 10) * 100
-        )
+    fun createOptions(): List<ProductOption> {
+        val list = mutableListOf<ProductOption>()
+        for (i in 1..10) {
+            val index = i % 2
+            list.add(createByTypeAndName(ProductOptionType.COMBINATION, OPTION_NAMES[index]))
+            list.add(createByTypeAndName(ProductOptionType.EXTRA, "추가 상품"))
+        }
+        return list
     }
+
+    private fun createByTypeAndName(type: ProductOptionType, name: String) = ProductOption(
+        type = type,
+        name = name,
+        value = RandomStringUtils.randomAlphabetic(5),
+        code = RandomStringUtils.randomAlphabetic(5),
+        price = Random.nextInt(1, 100) * 100
+    )
 
 }
