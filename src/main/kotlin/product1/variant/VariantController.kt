6d6b3e9175bb170eb80de6192f3e43f5c643af5e
hoya <hybriddevslim@gmail.com>
2022-08-05 14:15:02 +0900

    VariantController - 옵션 상품 조회 API 수정

--- src/main/kotlin/product1/variant/VariantController.kt
+++ src/main/kotlin/product1/variant/VariantController.kt
@@ -1,10 +1,12 @@
 package product1.variant
 
 import org.bson.types.ObjectId
+import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.server.ResponseStatusException
 
 @RestController
 @RequestMapping(value = ["/products"])
@@ -13,11 +15,15 @@ class VariantController(
 ) {
 
     /**
-     * 코드로 옵션 상품 조회
+     * 옵션 상품 조회
      * @param productId 상품 번호
-     * @param code '-' 구분자로 구성된  옵션 상품 코드
+     * @param query 옵션 상품 질의 필드
      */
-    @GetMapping(value = ["/{productId}/variants/code/{code}"])
-    suspend fun getVariantByCode(@PathVariable productId: ObjectId, @PathVariable code: String): Variant =
-        variantService.getVariantByCode(productId, code)
+    @GetMapping(value = ["/{productId}/variants/query"])
+    suspend fun getVariant(@PathVariable productId: ObjectId, query: VariantQuery): VariantView {
+        val variant = variantService.getVariantByCode(productId, query.code)
+        if(variant.stock < query.count) throw ResponseStatusException(HttpStatus.BAD_REQUEST)
+        return variant.toVariantView(query.count)
+    }
+
 }
--- src/test/kotlin/product1/controller/VariantControllerTests.kt
+++ src/test/kotlin/product1/controller/VariantControllerTests.kt
@@ -2,15 +2,15 @@ package product1.controller
 
 import com.ninjasquad.springmockk.MockkBean
 import io.mockk.coEvery
+import org.bson.types.ObjectId
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest
+import org.springframework.http.HttpStatus
 import org.springframework.test.web.reactive.server.WebTestClient
+import org.springframework.web.server.ResponseStatusException
 import product1.product.fixture.ProductFixture
-import product1.variant.Variant
-import product1.variant.VariantController
-import product1.variant.VariantFixture
-import product1.variant.VariantService
+import product1.variant.*
 
 @WebFluxTest(VariantController::class)
 class VariantControllerTests {
@@ -23,17 +23,63 @@ class VariantControllerTests {
     lateinit var variantService: VariantService
 
     @Test
-    fun getVariantByCode()  {
+    fun getVariantNotFound() {
         val product = ProductFixture.createProduct()
         val variants = VariantFixture.createVariants(product)
-        val variant = variants.first()
+        val variant = variants.first().copy(id = ObjectId.get())
+        val code = "dd"
+        coEvery {
+            variantService.getVariantByCode(product.id!!, code)
+        } throws ResponseStatusException(HttpStatus.NOT_FOUND)
+
+        webTestClient
+            .get()
+            .uri { builder ->
+                builder
+                    .queryParam("code", code)
+                    .queryParam("count", variant.stock)
+                    .path("/products/${product.id}/variants/query")
+                    .build()
+            }
+            .exchange()
+            .expectStatus().isNotFound
+
+    }
+
+    @Test
+    fun getVariant() {
+        val product = ProductFixture.createProduct()
+        val variants = VariantFixture.createVariants(product)
+        val variant = variants.first().copy(id = ObjectId.get())
         coEvery {
             variantService.getVariantByCode(product.id!!, variant.code)
         } returns variant
 
-        webTestClient.get().uri("/products/${product.id}/variants/code/${variant.code}")
+        webTestClient
+            .get()
+            .uri { builder ->
+                builder
+                    .queryParam("code", variant.code)
+                    .queryParam("count", variant.stock + 1)
+                    .path("/products/${product.id}/variants/query")
+                    .build()
+
+            }
+            .exchange()
+            .expectStatus().isBadRequest
+
+        webTestClient
+            .get()
+            .uri { builder ->
+                builder
+                    .queryParam("code", variant.code)
+                    .queryParam("count", variant.stock)
+                    .path("/products/${product.id}/variants/query")
+                    .build()
+
+            }
             .exchange()
             .expectStatus().isOk
-            .expectBody(Variant::class.java)
+            .expectBody(VariantView::class.java)
     }
 }
