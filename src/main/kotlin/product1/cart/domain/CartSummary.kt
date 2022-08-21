aac3859eb56adbc62f4fefd55ec303be407c27d2
hoya <hybriddevslim@gmail.com>
2022-08-21 13:57:35 +0900

    CartSummary 엔티티 수정: 생성자 로직 추가

--- src/main/kotlin/product1/cart/domain/CartSummary.kt
+++ src/main/kotlin/product1/cart/domain/CartSummary.kt
@@ -24,13 +24,24 @@ data class CartSummary(
      */
     val total: Int = subtotal + shippingAmount - discountAmount
 ) {
+
     companion object {
 
         /**
          * 장바구니 금액 요약 데이터 생성
          */
-        fun of(): CartSummary = CartSummary(
-            subtotal = 0,
+        fun of(subtotal: Int = 0): CartSummary = CartSummary(
+            subtotal = subtotal,
+            shippingAmount = 5000,
+            discountAmount = 0
+        )
+
+        /**
+         * 장바구니 금액 요약 데이터 생성
+         * @param lineItems 구매 항목
+         */
+        fun of(lineItems: List<LineItem>): CartSummary = CartSummary(
+            subtotal = lineItems.sumOf { it.sumOfTotal() },
             shippingAmount = 5000,
             discountAmount = 0
         )
