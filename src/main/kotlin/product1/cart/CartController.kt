5e63a9962e34b53ff5d46b6a332aae7ed6c2e19b
hoya <hybriddevslim@gmail.com>
2022-08-21 13:59:39 +0900

    CartController 추가: 장바구니 상품 담기

---
+++ src/main/kotlin/product1/cart/CartController.kt
@@ -0,0 +1,26 @@
+package product1.cart
+
+import org.bson.types.ObjectId
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.*
+import product1.cart.domain.Cart
+import product1.cart.dto.LineItemPayload
+import javax.validation.Valid
+
+@RestController
+@RequestMapping(value = ["/carts"])
+class CartController(
+    private val cartFacadeService: CartFacadeService
+) {
+
+    /**
+     * 장바구니 상품 담기
+     * @param employeeId 직원번호
+     * @param payload 구매 항목 필드 데이터
+     */
+    @PostMapping(value = ["/{employeeId}/lineItem"])
+    @ResponseStatus(value = HttpStatus.CREATED)
+    suspend fun addLineItem(@PathVariable employeeId: ObjectId, @Valid @RequestBody payload: LineItemPayload): Cart =
+        cartFacadeService.addLineItem(employeeId, payload)
+
+}
