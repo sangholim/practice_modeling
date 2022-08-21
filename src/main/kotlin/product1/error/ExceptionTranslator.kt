4e97267bd30300542fc7ef6c1a269b163d5188f6
hoya <hybriddevslim@gmail.com>
2022-08-21 14:22:18 +0900

    ExceptionTranslator 개선: 예외 로그 추가

--- src/main/kotlin/product1/error/ExceptionTranslator.kt
+++ src/main/kotlin/product1/error/ExceptionTranslator.kt
@@ -1,5 +1,6 @@
 package product1.error
 
+import org.slf4j.LoggerFactory
 import org.springframework.http.ResponseEntity
 
 import org.springframework.web.bind.annotation.ExceptionHandler
@@ -10,19 +11,29 @@ import org.springframework.web.server.ResponseStatusException
 @RestControllerAdvice
 class ExceptionTranslator {
 
+    private val logger = LoggerFactory.getLogger(javaClass)
+
     @ExceptionHandler(ResponseStatusException::class)
-    fun handleResponseStatusException(e: ResponseStatusException): ResponseEntity<ErrorBody> =
-        ResponseEntity.status(e.status).body(ErrorBody.create(e))
+    fun handleResponseStatusException(e: ResponseStatusException): ResponseEntity<ErrorBody> {
+        logger.error(e.stackTraceToString())
+        return ResponseEntity.status(e.status).body(ErrorBody.create(e))
+    }
 
     @ExceptionHandler(WebExchangeBindException::class)
-    fun handleWebExchangeBindException(e: WebExchangeBindException): ResponseEntity<ErrorBody> =
-        ResponseEntity.badRequest().body(ErrorBody.create(e))
+    fun handleWebExchangeBindException(e: WebExchangeBindException): ResponseEntity<ErrorBody> {
+        logger.error(e.stackTraceToString())
+        return ResponseEntity.badRequest().body(ErrorBody.create(e))
+    }
 
     @ExceptionHandler(IllegalArgumentException::class)
-    fun handleIllegalArgumentException(e: IllegalArgumentException): ResponseEntity<ErrorBody> =
-        ResponseEntity.badRequest().body(ErrorBody.create(e))
+    fun handleIllegalArgumentException(e: IllegalArgumentException): ResponseEntity<ErrorBody> {
+        logger.error(e.stackTraceToString())
+        return ResponseEntity.badRequest().body(ErrorBody.create(e))
+    }
 
     @ExceptionHandler(Exception::class)
-    fun handleException(e: Exception): ResponseEntity<ErrorBody> =
-        ResponseEntity.internalServerError().body(ErrorBody.create())
+    fun handleException(e: Exception): ResponseEntity<ErrorBody> {
+        logger.error(e.stackTraceToString())
+        return ResponseEntity.internalServerError().body(ErrorBody.create())
+    }
 }
