95b11af95cdeb23396a29749f3430ae60366c617
hoya <hybriddevslim@gmail.com>
2022-09-04 13:48:01 +0900

    CartFacadeService - 장바구니 구매 항목 담기 로직 수정

--- src/main/kotlin/product1/cart/CartFacadeService.kt
+++ src/main/kotlin/product1/cart/CartFacadeService.kt
@@ -9,7 +9,6 @@ import product1.cart.domain.Item
 import product1.cart.domain.LineItem
 import product1.cart.dto.LineItemPayload
 import product1.product.ProductService
-import product1.variant.Variant
 import product1.variant.VariantService
 
 @Service
@@ -21,8 +20,14 @@ class CartFacadeService(
 
     /**
      * 장바구니 상품 담기
-     * 항목이 없는 경우, 항목 추가
-     * 항목이 있는 경우, 수량만 추가
+     * 장바구니 조회
+     * 요청한 구매 항목의 상품 조회
+     * 요청한 구매 항목의 상품 옵션 조회
+     *
+     * 요청한 구매 항목이 업는 경우 - 장바구니에 구매항목을 추가한다
+     * 요청한 구매 항목이 있는 경우 - 장바구니에 구매항목을 업데이트한다
+     *
+     * 장바구니 구매항목이 변경된 경우 - 정산 요약을 업데이트 한다
      *
      * @param employeeId 고객 번호
      * @param payload 장바구니 구매 항목 필드
@@ -35,21 +40,13 @@ class CartFacadeService(
                 HttpStatus.NOT_FOUND
             )
         }
-        // 장바구니에 담는 옵션 상품들
+
         val items = payload.items.map { item ->
             val variant = variants.first { item.variantId == it.id }
             Item.of(product, variant, item.count)
         }
 
-        /**
-         * 구매 항목 없는 경우 - 구매 항목 추가
-         *
-         * 구매 항목 있는 경우 - 옵션 상품 담기
-         *  옵션 상품이 있는 경우 - 수량을 늘린다
-         *  옵션 상품이 없는 경우 - 옵션 상품을 추가한다
-         */
-        val lineItem = cart.findLineItem(payload.productId)?.addItems(items) ?:
-            return cartService.updateCart(cart.addLineItem(LineItem.of(product, items)))
-        return cartService.updateCart(cart.updateLineItem(lineItem))
+        val lineItem = cart.findLineItem(payload.productId)?.addItems(items) ?: LineItem.of(product, items)
+        return cartService.updateCart(cart.addLineItem(lineItem).updateSummary())
     }
 }
