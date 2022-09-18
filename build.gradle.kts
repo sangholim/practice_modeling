86fd911efe0e220b473e088ec666a7c8e615b740
hoya <hybriddevslim@gmail.com>
2022-09-18 14:54:52 +0900

    build.gradle.kts: ko-test, 코루틴 라이브러리 추가 및 수정

--- build.gradle.kts
+++ build.gradle.kts
@@ -34,17 +34,25 @@ dependencies {
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.1")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor:1.6.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core-jvm:1.6.4")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor:1.6.4")
     implementation ("org.jetbrains.kotlin:kotlin-stdlib")
     implementation ("org.testcontainers:testcontainers-bom:1.17.1")
 
-    testImplementation("io.mockk:mockk:1.12.4")
+    testImplementation ("io.kotest:kotest-runner-junit5:5.4.2")
+    testImplementation ("io.kotest:kotest-assertions-core:5.4.2")
+    testImplementation ("io.kotest:kotest-property:5.4.2")
+    testImplementation ("io.kotest.extensions:kotest-extensions-spring:1.1.2")
+    testImplementation ("io.kotest.extensions:kotest-extensions-testcontainers:1.3.4")
+    testImplementation ("io.kotest:kotest-runner-junit5-jvm:5.4.2")
+
+    testImplementation("io.mockk:mockk:1.12.7")
     testImplementation("com.ninja-squad:springmockk:3.1.1")
-    testImplementation ("org.junit.jupiter:junit-jupiter-api:5.8.2")
-    testRuntimeOnly ("org.junit.jupiter:junit-jupiter-engine:5.8.2")
+    testImplementation ("org.junit.jupiter:junit-jupiter-api:5.9.0")
+    testRuntimeOnly ("org.junit.jupiter:junit-jupiter-engine:5.9.0")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
-    testImplementation ("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.6.1")
+    testImplementation ("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.6.4")
     testImplementation ("org.testcontainers:mongodb:1.17.1")
     testImplementation ("org.testcontainers:rabbitmq:1.17.1")
     testImplementation ("org.testcontainers:testcontainers:1.16.3")
@@ -76,10 +84,9 @@ tasks.register("runVendor") {
     }
 }
 
-tasks.withType<Test> {
+tasks.withType<Test>().configureEach {
     useJUnitPlatform()
 }
-
 springBoot{
     this.mainClass.set("product1.ProductApplicationKt")
 }
