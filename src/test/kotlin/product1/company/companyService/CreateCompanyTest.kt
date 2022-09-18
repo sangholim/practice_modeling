99fbd314865144916aeb5fe17506c3706528ac8b
hoya <hybriddevslim@gmail.com>
2022-09-18 15:37:22 +0900

    companyService.CreateCompanyTest 추가 CompanyServiceTests 제거

---
+++ src/test/kotlin/product1/company/companyService/CreateCompanyTest.kt
@@ -0,0 +1,52 @@
+package product1.company.companyService
+
+import io.kotest.assertions.throwables.shouldThrowMessage
+import io.kotest.core.spec.style.BehaviorSpec
+import io.kotest.matchers.shouldBe
+import io.mockk.clearAllMocks
+import io.mockk.coEvery
+import io.mockk.mockk
+import product1.company.Company
+import product1.company.CompanyRepository
+import product1.company.CompanyService
+import product1.fixture.CompanyFixture
+
+class CreateCompanyTest : BehaviorSpec({
+    val companyRepository: CompanyRepository = mockk()
+    val companyService = CompanyService(companyRepository)
+    val payload =CompanyFixture.createCompanyPayload()
+    beforeTest {
+        clearAllMocks()
+    }
+
+    Given("동일한 증명서인 경우") {
+        Then("이미 가입한 증명서입니다. 예외 메세지 발생") {
+            coEvery { companyRepository.existsByCertificate(payload.certificate) } returns true
+            shouldThrowMessage("이미 가입한 증명서입니다.") {
+                companyService.createCompany(payload)
+            }
+        }
+    }
+
+    Given("동일한 조직명인 경우") {
+        Then("이미 가입한 조직명입니다. 예외 메세지 발생") {
+            coEvery { companyRepository.existsByCertificate(payload.certificate) } returns false
+            coEvery { companyRepository.existsByName(payload.name) } returns true
+            shouldThrowMessage("이미 가입한 조직명입니다.") {
+                companyService.createCompany(payload)
+            }
+        }
+    }
+
+    Given("조직 생성 성공") {
+        Then("Company 타입의 객체를 반환") {
+            val expected = Company.create(payload)
+            coEvery { companyRepository.existsByCertificate(payload.certificate) } returns false
+            coEvery { companyRepository.existsByName(payload.name) } returns false
+            coEvery { companyRepository.save(expected) } returns expected
+            val result = companyService.createCompany(payload)
+            result shouldBe expected
+        }
+    }
+
+})
