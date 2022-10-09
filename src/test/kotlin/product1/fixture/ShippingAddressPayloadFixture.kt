e29541f797d20504711f49868c78ad48c6fe4939
hoya <hybriddevslim@gmail.com>
2022-10-09 13:46:56 +0900

    ShippingAddressPayloadFixture.kt 추가

--- src/test/kotlin/product1/fixture/ShippingAddressPayloadFixture.kt
+++ src/test/kotlin/product1/fixture/ShippingAddressPayloadFixture.kt
@@ -6,6 +6,31 @@ import product1.shippingaddress.ShippingAddress
 import product1.shippingaddress.ShippingAddressPayload
 import java.time.Instant
 
+inline fun shippingAddressPayload(block: ShippingAddressPayloadBuilder.() -> Unit = {}) =
+    ShippingAddressPayloadBuilder().apply(block).build()
+
+class ShippingAddressPayloadBuilder {
+    var name: String = ""
+    var firstRecipient: String = ""
+    var secondRecipient: String? = null
+    var firstPhoneNumber: String = ""
+    var secondPhoneNumber: String? = null
+    var zipCode: String = ""
+    var line1: String = ""
+    var line2: String? = null
+
+    fun build() = ShippingAddressPayload(
+        name = name,
+        firstRecipient = firstRecipient,
+        secondRecipient = secondRecipient,
+        firstPhoneNumber = firstPhoneNumber,
+        secondPhoneNumber = secondPhoneNumber,
+        zipCode = zipCode,
+        line1 = line1,
+        line2 = line2
+    )
+}
+
 object ShippingAddressFixture {
 
     val id: ObjectId = ObjectId("507f1f77bcf86cd799439011")
