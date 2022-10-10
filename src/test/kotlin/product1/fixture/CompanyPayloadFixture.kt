a5c58df6ca5a610f3851b97b9894270aefb1202d
hoya <hybriddevslim@gmail.com>
2022-10-10 13:27:16 +0900

    CompanyPayloadFixture.kt 불필요 로직 제거

--- src/test/kotlin/product1/fixture/CompanyPayloadFixture.kt
+++ src/test/kotlin/product1/fixture/CompanyPayloadFixture.kt
@@ -1,9 +1,6 @@
 package product1.fixture
 
-import org.bson.types.ObjectId
-import product1.company.Company
 import product1.company.CompanyPayload
-import product1.company.CompanyView
 
 inline fun companyPayload(block: CompanyPayloadBuilder.() -> Unit = {}) =
     CompanyPayloadBuilder().apply(block).build()
@@ -17,53 +14,5 @@ class CompanyPayloadBuilder {
 
     fun build() = CompanyPayload(
         name, email, phoneNumber, certificate, address
-    )
-}
-
-object CompanyFixture {
-
-    val id: ObjectId = ObjectId("507f1f77bcf86cd799439011")
-    const val name = "테스트회사"
-    const val email = "test@test.com"
-    const val phoneNumber = "1112223333"
-    const val certificate = "testCertificate"
-    const val address = "테스트 동 테스트시"
-
-    fun createCompanyView(): CompanyView = CompanyView(
-        id = id,
-        name = name,
-        email = email,
-        phoneNumber = phoneNumber,
-        certificate = certificate,
-        address = address,
-        balance = 0
-    )
-
-    fun createInvalidCompanyPayload(): CompanyPayload = CompanyPayload(
-        name = "ㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁ",
-        email = "가@가.가",
-        phoneNumber = "가",
-        certificate = "",
-        address = ""
-    )
-
-    fun createCompanyPayload(): CompanyPayload = CompanyPayload(
-        name = name,
-        email = email,
-        phoneNumber = phoneNumber,
-        certificate = certificate,
-        address = address
-    )
-
-    fun createCompany(id: ObjectId? = null): Company = Company(
-        id = id,
-        name = name,
-        email = email,
-        emailVerified = true,
-        phoneNumber = phoneNumber,
-        phoneNumberVerified = true,
-        certificate = certificate,
-        address = address,
-        balance = 0
     )
 }
